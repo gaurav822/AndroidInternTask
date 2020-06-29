d2a4358c58cfdc0d7e69abfebcd319ecac085404
Gaurav Dahal <gauravdahal53@gmail.com>
2020-06-29 12:27:43 +0545

    Third commit

--- Task-3(Login & Registration Pages)/app/src/main/java/com/example/loginregisterapp/Registration.java
+++ Task-3(Login & Registration Pages)/app/src/main/java/com/example/loginregisterapp/Registration.java
@@ -2,24 +2,54 @@ package com.example.loginregisterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Registration extends AppCompatActivity {
 
-    EditText et,et2;
+    EditText et,et2,et3;
+    Button b1;
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String Name = "nameKey";
+    public static final String Phone = "phoneKey";
+    public static final String Email = "emailKey";
+
+    SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         et= (EditText) findViewById(R.id.userName);
-        et2 =(EditText) findViewById(R.id.passWord);
+        et2= (EditText) findViewById(R.id.emailId);
+        et3 =(EditText) findViewById(R.id.phoneNumber);
+        b1= (Button) findViewById(R.id.registerButton);
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String n=et.getText().toString();
+                String ph=et2.getText().toString();
+                String e=et3.getText().toString();
+
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                editor.putString(Name, n);
+                editor.putString(Phone, ph);
+                editor.putString(Email, e);
+                editor.commit();
+                Toast.makeText(Registration.this,"Thanks",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
-    public void Registration(View view) {
-
-    }
 }
